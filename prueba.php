452a405cf221a361423ce9f79a5ad87703d25527
energyop1 <paula.orejon02@gmail.com>
2023-03-10 13:12:10 +0100

    vale1

--- prueba.php
+++ prueba.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>PAULA.2</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
 <body>
     <?php
-        $numero= rand(1,100);
-
-        if ($numero%2){
-            echo "$numero Es impar";
-        }
-        else {
-            echo "$numero es par";
-        }
-        
+        <?php
+        $resultado = 0;
+        $tipo = gettype($resultado);
+        echo "Resultado vale: $resultado y es de tipo $tipo<br>";
+        $resultado2 = (double)$resultado;
+        $tipo = gettype($resultado2);
+        echo "y Resultado 2 vale: $resultado2 y es de tipo $tipo<br>";
+        $tipo = gettype($resultado);
+        echo "mientras Resultado vale: $resultado y es de tipo $tipo<br>";
+        ?>
         
     ?>
     
