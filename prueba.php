00ce92ec32db4615c10a0e09bd446d2f488e857a
energyop1 <paula.orejon02@gmail.com>
2023-03-10 13:13:01 +0100

    vale2

--- prueba.php
+++ prueba.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PAULA.2</title>
+    <title>Prueba.3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 </head>
 <body>
     <?php
-        <?php
         $resultado = 0;
         $tipo = gettype($resultado);
         echo "Resultado vale: $resultado y es de tipo $tipo<br>";
@@ -19,7 +18,6 @@
         echo "y Resultado 2 vale: $resultado2 y es de tipo $tipo<br>";
         $tipo = gettype($resultado);
         echo "mientras Resultado vale: $resultado y es de tipo $tipo<br>";
-        ?>
         
     ?>
     
