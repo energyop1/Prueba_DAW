4b1c6ee33b4f3c0f59fd89d31398be37889ca36c
energyop1 <paula.orejon02@gmail.com>
2023-03-13 09:54:42 +0100

    modificacion archivo prueba3

---
+++ prueba2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Prueba.3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body>
+    <?php
+        $resultado = 0;
+        $tipo = gettype($resultado);
+        echo "Resultado vale: $resultado y es de tipo $tipo<br>";
+        $resultado2 = (double)$resultado;
+        $tipo = gettype($resultado2);
+        echo "y Resultado 2 vale: $resultado2 y es de tipo $tipo<br>";
+        ?>
+    
+</body>
+</html>
